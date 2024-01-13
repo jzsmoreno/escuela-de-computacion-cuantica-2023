b0614a84892ea2b993ce0423a73f8daff0d4c370
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2024-01-12 22:51:42 -0600

    Update `.ipynb`
    * reformatting

--- dia05/utilities.py
+++ dia05/utilities.py
@@ -5,7 +5,7 @@ from qiskit_ibm_provider import least_busy
 from qiskit.tools.monitor import job_monitor
 from qiskit.visualization import plot_histogram, plot_bloch_multivector
 
-        
+
 def qft_rotations(circuit, n):
     """Performs qft on the first n qubits in circuit (without swaps)"""
     if n == 0:
@@ -13,21 +13,24 @@ def qft_rotations(circuit, n):
     n -= 1
     circuit.h(n)
     for qubit in range(n):
-        circuit.cp(pi/2**(n-qubit), qubit, n)
+        circuit.cp(pi / 2 ** (n - qubit), qubit, n)
     # At the end of our function, we call the same function again on
     # the next qubits (we reduced n by one earlier in the function)
     qft_rotations(circuit, n)
-    
+
+
 def swap_registers(circuit, n):
-    for qubit in range(n//2):
-        circuit.swap(qubit, n-qubit-1)
+    for qubit in range(n // 2):
+        circuit.swap(qubit, n - qubit - 1)
     return circuit
+
 
 def qft(circuit, n):
     """QFT on the first n qubits in circuit"""
     qft_rotations(circuit, n)
     swap_registers(circuit, n)
-    return circuit    
+    return circuit
+
 
 def inverse_qft(circuit, n):
     """Does the inverse QFT on the first n qubits in circuit"""
@@ -37,4 +40,4 @@ def inverse_qft(circuit, n):
     invqft_circ = qft_circ.inverse()
     # And add it to the first n qubits in our existing circuit
     circuit.append(invqft_circ, circuit.qubits[:n])
-    return circuit.decompose() # .decompose() allows us to see the individual gates
+    return circuit.decompose()  # .decompose() allows us to see the individual gates
